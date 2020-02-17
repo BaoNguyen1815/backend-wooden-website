8441e321ad721869c6d8a14810e77531dd7523d3
BaoNguyen1815 <baonguyendo1810@gmail.com>
2020-02-17 15:37:00 +0700

    phan trang va upload done

--- Route/ProductRoute.js
+++ Route/ProductRoute.js
@@ -1,32 +1,32 @@
 const express = require("express");
 const ProductRoute = express.Router();
 const Product = require("../Model/Product");
-const multer = require("multer")
-var upload = multer({
-  storage: multer.diskStorage({
-    destination: function(req, file, callback) {
-      callback(null, "../uploads");
-    },
-    filename: function(req, file, callback) {
-      callback(
-        null,
-        file.fieldname + "-" + Date.now() + path.extname(file.originalname)
-      );
-    }
-  }),
-  fileFilter: function(req, file, callback) {
-    var ext = path.extname(file.originalname);
-    if (ext !== ".png" && ext !== ".JPG" && ext !== ".gif" && ext !== ".jpeg") {
-      return callback(/*res.end('Only images are allowed')*/ null, false);
-    }
+const multer = require("multer");
+const fs = require("fs")
+// var upload = multer({
+//   storage: multer.diskStorage({
+//     destination: function(req, file, callback) {
+//       callback(null, "../uploads");
+//     },
+//     filename: function(req, file, callback) {
+//       callback(
+//         null,
+//         file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+//       );
+//     }
+//   }),
+//   fileFilter: function(req, file, callback) {
+//     var ext = path.extname(file.originalname);
+//     if (ext !== ".png" && ext !== ".JPG" && ext !== ".gif" && ext !== ".jpeg") {
+//       return callback(/*res.end('Only images are allowed')*/ null, false);
+//     }
 
-    callback(null, true);
-  }
-});
+//     callback(null, true);
+//   }
+// });
+const upload = multer({ dest: "images/" });
 // API Add Product
-ProductRoute.post("/",upload.single('image'), (req, res, next) => {
-  console.log(req.file)
-  console.log(req.body)
+ProductRoute.post("/", (req, res, next) => {
   Product.addProduct(req.body, err => {
     if (err) res.json(err);
     else
@@ -35,6 +35,24 @@ ProductRoute.post("/",upload.single('image'), (req, res, next) => {
         data: req.body
       });
   });
+});
+
+
+//API Upload
+ProductRoute.post("/upload",upload.single('image'), (req, res, next) => {
+  console.log(req.file)
+   const processedFile = req.file || {}; // MULTER xử lý và gắn đối tượng FILE vào req
+   let orgName = processedFile.originalname || ""; // Tên gốc trong máy tính của người upload
+   orgName = orgName.trim().replace(/ /g, "-");
+   const fullPathInServ = processedFile.path; // Đường dẫn đầy đủ của file vừa đc upload lên server
+   // Đổi tên của file vừa upload lên, vì multer đang đặt default ko có đuôi file
+   const newFullPath = `${fullPathInServ}-${orgName}`;
+   fs.renameSync(fullPathInServ, newFullPath);
+   res.send({
+     status: true,
+     message: "file uploaded",
+     fileNameInServer: newFullPath
+   });
 });
 
 // API Get all
